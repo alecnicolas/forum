0a4cec84cc929a408bcf1355d49a4bc3e7b3a0f4
alecnicolas <alecnmarcum@gmail.com>
2019-10-14 20:05:56 -0700

    response body changes

--- ForumServer/src/main/java/com/ForumServer/repositories/ForumPostRepository.java
+++ ForumServer/src/main/java/com/ForumServer/repositories/ForumPostRepository.java
@@ -2,11 +2,13 @@ package com.ForumServer.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ForumServer.entities.ForumPost;;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 @Repository
+@CrossOrigin(origins = "http://localhost:4200")
 public interface ForumPostRepository extends JpaRepository<ForumPost, Integer> {
 }
