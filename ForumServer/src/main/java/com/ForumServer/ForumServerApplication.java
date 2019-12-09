876bd3cf0911564413354f6d9216d28e7a4eaac2
alecnicolas <alecnmarcum@gmail.com>
2019-12-08 20:07:53 -0800

    rip juice

--- ForumServer/src/main/java/com/ForumServer/ForumServerApplication.java
+++ ForumServer/src/main/java/com/ForumServer/ForumServerApplication.java
@@ -2,12 +2,19 @@ package com.ForumServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ForumServerApplication {
+public class ForumServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumServerApplication.class, args);
-	}
+    }
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ForumServerApplication.class);
+    }
 
 }
